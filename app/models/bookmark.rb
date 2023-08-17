fd344f990893c01a4e383fbfa4a2360c27ae0389
jorge marques <jorgetmarques@gmail.com>
2023-08-17 18:32:38 +0100

    watch list. mostly done. needs style

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,5 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :comment, presence: true, length: { minimum: 6 }
-  validates :movie_id, uniqueness: { scope: :list_id }
+  validates :movie_id, uniqueness: { scope: :list_id } # validar a combo movie e list para não repetir filmes na mesma lista
 end
